e8632580e4c6337ef4cbfe8dec859d34cd9890e2
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2024-11-09 20:59:17 +0530

    🐞 Fix truncated types

--- src/utils/get-pretty-type.ts
+++ src/utils/get-pretty-type.ts
@@ -11,6 +11,8 @@ export async function getPrettyType(type: string, document: vscode.TextDocument)
         `
 type ${randomId}_Prettify<T> = T extends Function | string | number ? T: {[P in keyof T]: ${randomId}_Prettify<T[P]>};
 type ${randomId}_Result = ${randomId}_Prettify<${type}>;
+type ${randomId}_Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
+type ${randomId}_Exact = ${randomId}_Equal<${type}, ${randomId}_Result>;
     `;
 
     const tempFilePath = path.join(
@@ -23,19 +25,29 @@ type ${randomId}_Result = ${randomId}_Prettify<${type}>;
 
     const tempDocument = await vscode.workspace.openTextDocument(tempFilePath);
     const documentLastLineIndex = document.lineCount - 1;
-    const characterOffset = "type ".length; // Index where the `Result` type starts
+    const characterOffset = "type ".length; // Index where `Result`/`Exact` types start
 
-    const quickInfo = await getQuickInfo(
-        tempDocument,
-        new vscode.Position(documentLastLineIndex + 2, characterOffset),
-    );
+    const [quickInfoForResult, quickInfoForExact] = await Promise.all([
+        getQuickInfo(
+            tempDocument,
+            new vscode.Position(documentLastLineIndex + 2, characterOffset),
+        ),
+        getQuickInfo(
+            tempDocument,
+            new vscode.Position(documentLastLineIndex + 4, characterOffset),
+        ),
+    ]);
 
     // Delete the temporary file after getting the quick info
     fs.unlinkSync(tempFilePath);
 
-    if (!quickInfo) {
+    // `quickInfoForExact` is to check if the computed prettified type is same as the original type
+    if (!quickInfoForResult || !quickInfoForExact?.displayString.includes("true")) {
         return;
     }
 
-    return extractQuickInfoParts(quickInfo.displayString, quickInfo.kind)?.type;
+    return extractQuickInfoParts(
+        quickInfoForResult.displayString,
+        quickInfoForResult.kind,
+    )?.type;
 }
