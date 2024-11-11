5f4bee9845468c8d5e1167c1f49208c96470e2bb
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2024-11-11 14:59:58 +0530

    🧩 Get code from prettify-string.ts file

--- src/utils/get-pretty-type.ts
+++ src/utils/get-pretty-type.ts
@@ -3,16 +3,19 @@ import * as fs from "fs";
 import * as path from "path";
 import { getQuickInfo } from "./get-quick-info";
 import { extractQuickInfoParts } from "./extract-quick-info-parts";
+import { PRETTIFY_STRING } from "../types/prettify-string";
+
+function getCodeForPrettifying(type: string, randomId: string) {
+    return `${PRETTIFY_STRING.replace(/Prettify/g, `${randomId}_Prettify`)}
+
+export {};
+type ${randomId}_UglyType = ${type};
+type ${randomId}_PrettyType = ${randomId}_Prettify<${randomId}_UglyType>;`;
+}
 
 export async function getPrettyType(type: string, document: vscode.TextDocument) {
     const randomId = `PrettyTypeScriptTypes_${crypto.randomUUID().replace(/-/g, "_")}`;
-    const code =
-        document.getText() +
-        `
-export {};
-type ${randomId}_UglyType = ${type};
-type ${randomId}_Prettify<T> = T extends Function | string | number ? T: {[P in keyof T]: ${randomId}_Prettify<T[P]>};
-type ${randomId}_PrettyType = ${randomId}_Prettify<${randomId}_UglyType>;`;
+    const code = `${document.getText()}\n${getCodeForPrettifying(type, randomId)}`;
 
     const tempFilePath = path.join(
         path.dirname(document.fileName),
@@ -23,11 +26,14 @@ type ${randomId}_PrettyType = ${randomId}_Prettify<${randomId}_UglyType>;`;
     fs.writeFileSync(tempFilePath, code);
 
     const tempDocument = await vscode.workspace.openTextDocument(tempFilePath);
-    const characterOffset = "type ".length; // Index where `PrettyType` type starts
+    const lineIndex = tempDocument.lineCount - 1;
+    const characterIndex = tempDocument
+        .lineAt(lineIndex)
+        .text.indexOf(`${randomId}_PrettyType`);
 
     const quickInfo = await getQuickInfo(
         tempDocument,
-        new vscode.Position(tempDocument.lineCount - 1, characterOffset),
+        new vscode.Position(lineIndex, characterIndex),
     );
 
     // Delete the temporary file after getting the quick info
